725082055f5e6a74685163f5a33fd3a6988dabc2
Kaue <kaueoliveira2.0@outlook.com>
2025-08-23 21:26:40 -0300

    Implementado os construtores do Smartphone. Heranças no Nokia e Iphone para utilizar os contrutores e métodos. Método InstalarAplicativo() sobreescrito em ambas classes filhas do Smartphone. Devidos testes realizados no Program.cs.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Celular Nokia: ");
+Nokia nokia = new Nokia(numero: "123456789", modelo: "Nokia 3310", imei: "IMEI123456", memoria: 32);
+nokia.InstalarAplicativo("WhatsApp");
+nokia.Ligar();
+nokia.ReceberLigacao();
+
+Console.WriteLine("-------------------------");
+
+Console.WriteLine("Celular Iphone: ");
+Iphone iphone = new Iphone(numero: "234567890", modelo: "Iphone 15", imei: "IMEI234567", memoria: 128);
+iphone.InstalarAplicativo("Instagram");
+iphone.ReceberLigacao();
+iphone.Ligar();
